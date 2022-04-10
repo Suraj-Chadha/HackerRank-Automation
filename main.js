cc9b8bdc480a350dde863f0c70523f58947874ef
Suraj-Chadha <s.chadha234@gmail.com>
2022-04-10 09:24:20 +0530

    :fire: Automation logs in to hackerrank successfully pushed

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
 const url = "https://www.hackerrank.com/auth/login";
+const {email,password} = require("./secrets");
+let cTab;
 let browserWillBeOpenedPromise = puppeteer.launch({
     headless:false,
     defaultViewport: null,
@@ -11,7 +13,29 @@ browserWillBeOpenedPromise.then(function(browser){
     return allTabsArrPromise;
 })
 .then(function(allTabsArr){
-    let cTab = allTabsArr[0];
+    cTab = allTabsArr[0];
     // console.log("in current Tab---->",cTab);
     let OpenHackerRankLoginPagePromise = cTab.goto(url);
+    return OpenHackerRankLoginPagePromise;
+})
+.then(function(){
+    console.log("inside login screen");
+    let emailWillBeTypedPromise = cTab.type("#input-1",email,{delay:100});
+    return emailWillBeTypedPromise;
+})
+.then(function(){
+    console.log("email was typed successfully");
+    let passwordWillBeTypedPromise = cTab.type("#input-2",password,{delay:100});
+    return passwordWillBeTypedPromise;
+})
+.then(function(){
+    console.log("password was typed successfully");
+    let submitButtonWillBeClickedPromise = cTab.click(".auth-button");
+    return submitButtonWillBeClickedPromise;
+})
+.then(function(){
+    console.log("logged in successfully");
+})
+.catch(function(err){
+    console.log(err);
 })
